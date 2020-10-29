a6f54933cd2eb0779cffb4d846bf58bcc1d622d0
WesleyVitor <wesleydemorais@outlook.com.br>
2020-10-29 14:04:03 -0300

    feat: secure to password and action index implementation to home controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
-  validates_presence_of :email, :full_name, :location, :password
-  validates_confirmation_of :password
+  has_secure_password
+  validates_presence_of :email, :full_name, :location
   validates_length_of :bio, :minimum => 10, :allow_blank => false
   validates_format_of :email, :with => /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   validates :email, :uniqueness => true
