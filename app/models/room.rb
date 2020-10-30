4f7d137ffc96c8e4f9472c76f92f28898e038cec
WesleyVitor <wesleydemorais@outlook.com.br>
2020-10-30 18:25:49 -0300

    feat: create new style and layouts to pages

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,5 +1,8 @@
 class Room < ApplicationRecord
 
+  validates_presence_of :title, :location, :description
+  validates_length_of :description , :minimum => 10, :allow_blank => false
+
   def complete_name
     "#{title}, #{location}"
   end
