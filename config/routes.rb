10187458ea5f3071c73009c0b5fc78cd744cff2f
WesleyVitor <wesleydemorais@outlook.com.br>
2020-11-01 23:30:19 -0300

    refactor: apply i18n on templates

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+  
   resources :rooms
   resources :users
 
+  resource :user_confirmation, :only => [:show]
+  
+  
   root :to => "home#index"
+  
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
